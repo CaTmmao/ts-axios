8918a48f60631ba82cf97b34bab55f37a9fdad68
catmmao <catmmao@gmail.com>
2020-08-30 11:55:03 +0800

    chore:(examples) 测试 post 方法请求体的处理

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,66 +1,85 @@
+import axios from '../../src/index'
+
 /**
  * 测试发送请求前的 url 拼接功能
  */
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: ['bar', 'baz']
+//   }
+// })
 
-import axios from '../../src/index'
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: {
+//       bar: 'baz'
+//     }
+//   }
+// })
 
+// const date = new Date()
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     date
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: '@:$, '
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: 'bar',
+//     baz: null
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get#hash',
+//   params: {
+//     foo: 'bar'
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get?foo=bar',
+//   params: {
+//     bar: 'baz'
+//   }
+// })
+
+/**
+ * 测试发送请求前的 data 参数处理功能
+ */
 axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: ['bar', 'baz']
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
   }
 })
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: {
-      bar: 'baz'
-    }
-  }
-})
-
-const date = new Date()
+const arr = new Int32Array([21, 31])
 
 axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    date
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: '@:$, '
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: 'bar',
-    baz: null
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get#hash',
-  params: {
-    foo: 'bar'
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get?foo=bar',
-  params: {
-    bar: 'baz'
-  }
+  method: 'post',
+  url: '/base/buffer',
+  data: arr
 })
