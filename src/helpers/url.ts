887b10283f561623e4281bb3552d769b01303bed
catmmao <catmmao@gmail.com>
2020-08-30 11:25:44 +0800

    improvement:(helpers)仅判断是否为普通对象
    typeof 判断结果中，数组和 formdata 也是对象，这里需要改为判断是否是普通对象

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -2,7 +2,7 @@
  * 存放 axios url 相关的辅助函数
  */
 
-import { isDate, isObject } from './util'
+import { isDate, isPlainObject } from './util'
 
 function encode(val: string): string {
   return encodeURIComponent(val)
@@ -39,7 +39,7 @@ export function buildURL(url: string, params?: any): string {
     values.forEach(val => {
       if (isDate(val)) {
         val = val.toISOString()
-      } else if (isObject(val)) {
+      } else if (isPlainObject(val)) {
         val = JSON.stringify(val)
       }
 
--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -8,6 +8,16 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
+/**
+ * 判断是否是对象（包括 formdata、数组等）
+ */
 export function isObject(val: any): val is Object {
   return val !== null && typeof val === 'object'
+}
+
+/**
+ * 判断是否是普通对象（仅代表 {a:1} 这样的普通对象）
+ */
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
 }
