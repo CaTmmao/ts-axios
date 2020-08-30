1f39b36e6d7efa7965df0e05b1fee50e319efbce
catmmao <catmmao@gmail.com>
2020-08-30 16:32:17 +0800

    heat(xhr): 在发送请求时设置请求头

--- src/xhr.ts
+++ src/xhr.ts
@@ -2,10 +2,22 @@
 
 import { AxiosRequestConfig } from './types/index'
 export default function xhr(config: AxiosRequestConfig): void {
-  const { url, method = 'GET', data = null } = config
+  const { url, method = 'GET', data = null, headers } = config
 
   let request = new XMLHttpRequest()
 
   request.open(method.toLocaleUpperCase(), url) // 初始化一个请求
+
+  // 设置请求头
+  if (headers) {
+    Object.keys(headers).forEach(name => {
+      // 没有请求体，就不设置 content-type
+      if (!data && name.toLowerCase() === 'content-type') {
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+  }
+
   request.send(data) // 发送请求
 }
