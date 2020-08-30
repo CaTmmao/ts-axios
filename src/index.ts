566e4c68bf99992c10881dacc289de556d16b206
catmmao <catmmao@gmail.com>
2020-08-30 10:41:14 +0800

    feat:发送请求前拼接url

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
 // axios 库入口文件
 
 import { AxiosRequestConfig } from './types/index'
+import { buildURL } from './helpers/url'
 import xhr from './xhr'
 
+/**
+ * 处理请求地址
+ * @param config 请求配置
+ */
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+/**
+ * 发送请求前处理请求配置
+ * @param config 请求配置
+ */
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+}
+
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
 }
 
