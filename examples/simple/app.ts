dee186fadba4d1f8f57c58385f04685b5647b04a
catmmao <catmmao@gmail.com>
2020-09-02 10:16:21 +0800

    chore(examples): 测试拦截器功能

--- examples/simple/app.ts
+++ examples/simple/app.ts
@@ -1,10 +1,25 @@
 import axios from '../../src/index'
 
+axios.interceptors.request.use(config => {
+  config.headers.test += '11'
+  return config
+})
+
+axios.interceptors.response.use(response =>{
+  response.data = 'tesat'
+  return response
+})
+
 axios({
   method: 'get',
   url: '/simple/get',
   params: {
     a: 1,
     b: 2
+  },
+  headers: {
+    test: '2'
   }
+}).then(res => {
+  console.log(res)
 })
